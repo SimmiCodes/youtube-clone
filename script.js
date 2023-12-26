c39d09bb519b3f48616a3fb82d93cb98e1e159d5
SimmiCodes <ssiimmmmii@gmail.com>
2023-12-26 13:15:56 +0530

    Deployment preparation

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-const vedioCardContainer = document.querySelector('.video-container');
+const videoCardContainer=document.querySelector('.video-container');
 
-let api_key = "AIzaSyAEwL1eIor7nqinmj8SME7BlN6wQbr56pY";
-let vedio_http = "https://www.googleapis.com/youtube/v3/videos?";
-let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+let api_key="AIzaSyDdbEHGcFWSCHiN1sqcX0FY9gcE7PyqfPQ";
+let video_http="https://www.googleapis.com/youtube/v3/videos?";
+let channel_http="https://www.googleapis.com/youtube/v3/channels?"
 
-
-// api call 
-fetch(vedio_http + new URLSearchParams({
-    key : api_key,
-    part :'snippet',
-    chart : 'mostPopular',
-    maxResults : 75,
-    regionCode : 'IN'
+fetch(video_http + new URLSearchParams({
+    key:api_key,
+    part:'snippet',
+    chart:'mostPopular',
+    maxResults:44,
+    regionCode:"IN"
 }))
 
-.then(res => res.json())
-.then(data => {
-   console.log(data);
-      data.items.forEach(item => {
+.then(res=>res.json())
+.then(data=>{
+    // console.log(data);
+    data.items.forEach(item=>{
         getChannelIcon(item);
-      })
+    })
 })
-.catch(err => console.log(err));
+.catch(err=>console.log(err));
 
-const getChannelIcon = (vedio_data) => {
-      fetch(channel_http + new URLSearchParams({
-        key : api_key,
-        part :'snippet',
-        id : vedio_data.snippet.channelId
-      }))
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        vedio_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
-        makeVedioCard(vedio_data);
-      })
+const getChannelIcon=(video_data)=>{
+    fetch(channel_http+ new URLSearchParams({
+        key:api_key,
+        part:'snippet',
+        id:video_data.snippet.channelId
+    }))
+    .then(res=>res. json())
+    .then(data=>{
+        video_data.channelTumbnail=data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+
+    const makeVideoCard=(data)=>{
+        videoCardContainer.innerHTML +=`
+        
+        <div class="video" onclick="location.href='https://youtube.com/watch?v=${data.id}'">
+            <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+            <div class="content">
+                <img src="${data.channelTumbnail}" class="channel-icon" alt="">
+                <div class="info">
+                    <h4 class="title">${data.snippet.title}</h4>
+                    <p class="channel-name">${data.snippet.channelTitle}</p>
+                </div>
+
+            </div>
+        </div>
+        `;
+    }
+
+
 }
 
-const makeVedioCard = (data) => {
-    vedioCardContainer.innerHTML += `
-    <div class="vedio" onclick ="location.href = 'https://youtube.com/watch?v=${data.id}'">
-         <img src="${data.snippet.thumbnails.high.url}" heigth = 300px width = 300px class="thumbnail" alt="channel thumbnail">
-           <div class="content">
-              <img src="${data.channelThumbnail}" class="channel-icon" alt="channel icon">
-                  <div class="info">
-                     <h4 class="title">${data.snippet.title}</h4>
-                     <p class="channel-name">${data.snippet.channelTitle}</p>
-                  </div> 
-                 </div>
-                </div>
-                `;
-            }
 
-  const searchInput = document.querySelector('.search-bar');
-  const searchBtn = document.querySelector('.search-btn');
-  let searchLink ="https://www.youtube.com/results?search_query=";    
-  
-  searchBtn.addEventListener('click' , () => {
+const searchInput=document.querySelector('.search-bar');
+const serachBtn=document.querySelector('.search-btn');
+let searchLink="https://www.youtube.com/results?search_query=";
+
+serachBtn.addEventListener('click',()=>{
     if(searchInput.value.length){
-        location.href = searchLink + searchInput.value;
+        location.href=searchLink+searchInput.value;
     }
-  })
+})
+
+
+
+
+
